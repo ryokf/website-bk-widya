27430201c72a0d1db8df6638068931c0c1ada009
ryokf <ryokhrisnaf@gmail.com>
2024-06-28 10:42:12 +0700

    ✨feature:

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/js/app.jsx')
     @vite('resources/css/app.css')
     @inertiaHead
